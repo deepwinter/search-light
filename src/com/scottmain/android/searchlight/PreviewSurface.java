bc56ec9c9562087b89daa3e9430199a2eab9316e
Deepwinter <deepwinter@winterroot.net>
2013-05-17 16:10:43 -0700

    Made preview surface public

--- src/com/scottmain/android/searchlight/PreviewSurface.java
+++ src/com/scottmain/android/searchlight/PreviewSurface.java
@@ -30,7 +30,7 @@ import android.view.SurfaceView;
 import java.io.IOException;
 import java.util.List;
 
-class PreviewSurface extends SurfaceView implements SurfaceHolder.Callback {
+public class PreviewSurface extends SurfaceView implements SurfaceHolder.Callback {
 	private final static String TAG = "PreviewSurface";
     SurfaceHolder mHolder;
     Context mContext;
